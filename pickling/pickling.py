35fa07a0a9f81270e98f0ea827d9f8744254e326
Andrey Kropotin <tork713@gmail.com>
2019-04-23 16:00:05 +0700

    pickling

--- pickling/pickling.py
+++ pickling/pickling.py
@@ -1,1 +1,19 @@
+import pickle
+
+#Имя файла в котором мы сохраним объект
+shoplistfile = 'shoplist.data'
+#Список покупок
+shoplist = ['яблоки','манго','морковь']
+
+#Запись в файл
+f = open(shoplistfile,'wb') #Открываем файл для записи
+pickle.dump(shoplist,f) #Помещаем (сбрасываем - "dump") объект в файл
+f.close()
+
+del shoplist  #Уничтожаем переменную "shoplist"
+
+#Считываем из хранилища
+f = open(shoplistfile,'rb')
+storedlist = pickle.load(f) #Выгружаем объект из файла
+print(storedlist)
 
